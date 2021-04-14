94e835a726ff7bbf366c73245e925f072fe8c7e7
laorange <2625835752@qq.com>
2021-04-14 10:55:47 +0800

    v1.8 更正对角线模式下，g和h的计算方法

--- README.md
+++ README.md
@@ -12,11 +12,38 @@ Modeling_and_Simulation2021
 
 > 可选择沿对角线方向移动(2d变为8向, 3d变为26向)，但在以下示例中未开启该选项，2d为前后左右4个方向（3d为前后左右上下6个方向）
 
-## 2维情况
+源代码：[main.py](https://github.com/laorange/Modeling_and_Simulation2021/blob/main/main.py)
+
+更新日志：[log.md](https://github.com/laorange/Modeling_and_Simulation2021/blob/main/log.md)
+
+## 使用说明
+
+1. 打开命令行``cmd`` 或 ``Anaconda Prompt``
+
+2. 转到程序所在的文件夹下
+
+3. 安装所需第三方库
+
+   ```
+   pip install pandas
+   pip install plotly
+   ```
+
+4. 输入``python main.py -h``可查看所有参数的说明
+
+5. 示例：
+
+   ![image-20210414095631207](README_image/image-20210414095631207.png)
+
+## 结果
+
+**地图大小 100 × 100**
+
+### 2维情况
 
 (0, 0) → (80, 80) : 用时 43.03秒
 
-### 障碍物布局
+#### 障碍物布局
 
 ```python
 if x > 10 and y == 50 - x:
@@ -39,23 +66,23 @@ if x == 70 and 50 <= y <= 70:
 
 ![image-20210413152325940](README_image/image-20210413152325940.png)
 
-### 路径图
+#### 路径图
 
 ![image-20210413152449797](README_image/image-20210413152449797.png)
 
 ![image-20210413152646004](README_image/image-20210413152646004.png)
 
-### 迭代过程图
+#### 迭代过程图
 
 ![image-20210413152801304](README_image/image-20210413152801304.png)
 
 ![image-20210413152855198](README_image/image-20210413152855198.png)
 
-## 3维情况
+### 3维情况
 
 (0, 0, 0) → (80, 80, 80) : 用时 532.22秒
 
-### 障碍物布局
+#### 障碍物布局
 
 ```python
 if y == 20 - x and z < 60:
@@ -70,7 +97,7 @@ if x + y + z == 160 and 40 <= x <= 60 and 40 <= y <= 60 and 40 <= z <= 80:
 
 ![image-20210413154016518](README_image/image-20210413154016518.png)
 
-### 路径图
+#### 路径图
 
 ![image-20210413154320967](README_image/image-20210413154320967.png)
 
@@ -78,7 +105,7 @@ if x + y + z == 160 and 40 <= x <= 60 and 40 <= y <= 60 and 40 <= z <= 80:
 
 ![image-20210413154609073](README_image/image-20210413154609073.png)
 
-### 迭代过程图
+#### 迭代过程图
 
 ![image-20210413154733328](README_image/image-20210413154733328.png)
 
@@ -87,3 +114,11 @@ if x + y + z == 160 and 40 <= x <= 60 and 40 <= y <= 60 and 40 <= z <= 80:
 ![image-20210413154854911](README_image/image-20210413154854911.png)
 
 ![image-20210413155013934](README_image/image-20210413155013934.png)
+
+## 其他说明
+
+v1.7以后，地图默认大小改为20×20
+
+下图的识别仅需0.15秒
+
+![image-20210414093516055](README_image/image-20210414093516055.png)
--- main.py
+++ main.py
@@ -1,57 +1,47 @@
-import pandas as pd
-import plotly
-import plotly.express as px
-
 from time import perf_counter
 import random
 from threading import Thread
 import argparse
+import pandas as pd
+import plotly
+import plotly.express as px
 
 
 # TODO: 在此处设置障碍物的位置
 def this_point_is_an_obstacle(x: int, y: int, z: int):
     OBSTACLE = False
-    # 2d obstacle
-    if not opt.use_3d:
-        if x > 10 and abs(y - 50 + x) <= 2:
+    if not opt.use_3d:  # ↓ 2d obstacle
+        if x > 2 and abs(y - 10 + x) < 2:
             OBSTACLE = True
-        if x < 50 and abs(y - 60 + x) <= 2:
+        if x < 10 and abs(y - 15 + x) < 2:
             OBSTACLE = True
-        if x > 40 and abs(y - 70 + x) <= 2:
+        if abs(y - 13) < 4 and abs(x - 13) < 4:
             OBSTACLE = True
-        if abs(x - 50) <= 2 and y > 40:
+    else:  # ↓ 3d obstacle
+        if abs(y - 10 + x) < 2 and z < 12:
             OBSTACLE = True
-        if abs(x - 40) <= 2 and 30 <= y < 80:
+        if abs(y - 20 + x) < 2 and 12 <= z:
             OBSTACLE = True
-        if 60 <= x <= 70 and abs(y - 120 + x) <= 2:
-            OBSTACLE = True
-        if 50 <= x <= 70 and abs(y - 140 + x) <= 2:
-            OBSTACLE = True
-        if abs(x - 70) <= 2 and 50 <= y <= 70:
-            OBSTACLE = True
-    # 3d obstacle
-    else:
-        if abs(y - 40 + x) <= 2 and z < 60:
-            OBSTACLE = True
-        if abs(y - 60 + x) <= 2 and 60 <= z <= 90:
-            OBSTACLE = True
-        if abs(x + y + z - 160) <= 2 and 40 <= y <= 60 and 40 <= z <= 80:
+        if abs(x + y + z - 44) < 2 and 8 <= x <= 18 and 8 <= y <= 18 and 8 <= z <= 16:
             OBSTACLE = True
     return OBSTACLE
 
 
 class Point:
-    def __init__(self, x: int, y: int, z: int, g: int = 0, route=None, t: int = 0):
+    def __init__(self, x: int, y: int, z: int, g: float or int = 0.0, route=None, t: int = 0):
         self.x = x
         self.y = y
         self.z = z
         self.g = g  # g: 步数
 
-        # h: 距离终点的Manhattan distance: h(n) = D ∗ (dx + dy + dz)
-        if opt.use_3d:
+        if opt.use_3d:  # h 到终点的距离
             self.h = abs(opt.end_x - self.x) + abs(opt.end_y - self.y) + abs(opt.end_z - self.z)
+            if opt.oblique:
+                self.h = ((opt.end_x - self.x) ** 2 + (opt.end_y - self.y) ** 2 + (opt.end_z - self.z) ** 2) ** 0.5
         else:
             self.h = int(abs(opt.end_x - self.x) + abs(opt.end_y - self.y))
+            if opt.oblique:
+                self.h = ((opt.end_x - self.x) ** 2 + (opt.end_y - self.y) ** 2) ** 0.5
 
         self.f = self.g + self.h
 
@@ -71,10 +61,10 @@ class Point:
 # 在图中画出障碍物(点过多可能会导致图像加载失败)
 def record_all_obstacle_on_the_map():
     global obstacle_points
-    for x in range(opt.map_x):
-        for y in range(opt.map_y):
+    for x in range(opt.map_x + 1):
+        for y in range(opt.map_y + 1):
             if opt.use_3d:
-                for z in range(opt.map_z):
+                for z in range(opt.map_z + 1):
                     if this_point_is_an_obstacle(x, y, z):
                         obstacle_points.append(Point(x, y, z))
             else:
@@ -86,31 +76,32 @@ def record_all_obstacle_on_the_map():
 class OperationalPoint(Point):
     def move(self, axis: str):
         try:
+            distance = 0
+            x_new = self.x
+            y_new = self.y
+            z_new = self.z
             if "x" in axis:
+                distance += 1
                 change = 1
                 if "-x" in axis:
                     change = -1
                 x_new = self.x + change
-            else:
-                x_new = self.x
             if "y" in axis:
+                distance += 1
                 change = 1
                 if "-y" in axis:
                     change = -1
                 y_new = self.y + change
-            else:
-                y_new = self.y
             if "z" in axis:
+                distance += 1
                 change = 1
                 if "-z" in axis:
                     change = -1
                 z_new = self.z + change
-            else:
-                z_new = self.z
             if x_new > opt.map_x or x_new < 0 or y_new > opt.map_y or y_new < 0 or z_new > opt.map_z or z_new < 0:
                 raise Exception(f"当前坐标：({x_new}, {y_new}, {z_new})，超出边界")
             else:
-                g_new = self.g + 1
+                g_new = self.g + distance ** 0.5
                 if this_point_is_an_obstacle(x_new, y_new, z_new):
                     raise Exception(f"当前坐标：({x_new}, {y_new}, {z_new})，遇到障碍物")
                 new_route = self.route[:]
@@ -121,7 +112,7 @@ class OperationalPoint(Point):
                     computed_points.append(new_point)
                 else:
                     new_point_index = computed_points_lists.index(new_point.to_list())
-                    if computed_points[new_point_index].f > new_point.f:
+                    if computed_points[new_point_index].g > new_point.g:
                         computed_points[new_point_index] = new_point
         except Exception as e:
             pass  # print(e)
@@ -204,14 +195,6 @@ def visualize(route_mode: bool = True):
         x_ls.append(last_best_point.x)
         y_ls.append(last_best_point.y)
         z_ls.append(last_best_point.z)
-    # 画计算到的点，调试用
-    # for computed_point in computed_points:
-    #     if computed_point.to_list() not in closed_points_lists:
-    #         # t_ls.append(10 * computed_points_lists.index(computed_point) + 1000)
-    #         t_ls.append(10 * computed_point.f + 1000)
-    #         x_ls.append(computed_point.x)
-    #         y_ls.append(computed_point.y)
-    #         z_ls.append(computed_point.z)
     for obstacle_point in obstacle_points:
         t_ls.append(-100)
         x_ls.append(obstacle_point.x)
@@ -232,22 +215,22 @@ if __name__ == '__main__':
     parser.add_argument('--use-3d', action='store_true', help='维度是否为3维')
     parser.add_argument('--debug', action='store_true', help='调试模式，可查看障碍物&起点终点位置')
     parser.add_argument('--oblique', action='store_true', help='是否可以沿对角线方向移动 (单线/单层障碍物会被穿过，除非至少与两轴平行)')
-    parser.add_argument('--map-x', nargs='?', type=int, default=100, metavar='int', help='地图长度')
-    parser.add_argument('--map-y', nargs='?', type=int, default=100, metavar='int', help='地图宽度')
-    parser.add_argument('--map-z', nargs='?', type=int, default=100, metavar='int', help='地图高度')
+    parser.add_argument('--map-x', nargs='?', type=int, default=20, metavar='int', help='地图长度')
+    parser.add_argument('--map-y', nargs='?', type=int, default=20, metavar='int', help='地图宽度')
+    parser.add_argument('--map-z', nargs='?', type=int, default=20, metavar='int', help='地图高度')
     parser.add_argument('--start-x', nargs='?', type=int, default=0, metavar='int', help='起点x轴坐标')
     parser.add_argument('--start-y', nargs='?', type=int, default=0, metavar='int', help='起点y轴坐标')
     parser.add_argument('--start-z', nargs='?', type=int, default=0, metavar='int', help='起点z轴坐标')
-    parser.add_argument('--end-x', nargs='?', type=int, default=80, metavar='int', help='终点x轴坐标')
-    parser.add_argument('--end-y', nargs='?', type=int, default=80, metavar='int', help='终点y轴坐标')
-    parser.add_argument('--end-z', nargs='?', type=int, default=80, metavar='int', help='终点z轴坐标')
+    parser.add_argument('--end-x', nargs='?', type=int, default=20, metavar='int', help='终点x轴坐标')
+    parser.add_argument('--end-y', nargs='?', type=int, default=20, metavar='int', help='终点y轴坐标')
+    parser.add_argument('--end-z', nargs='?', type=int, default=20, metavar='int', help='终点z轴坐标')
     opt = parser.parse_args()
 
     if not opt.use_3d and opt.start_z != opt.end_z:
         opt.end_z = opt.start_z
         print("2维模式下，起点和终点的Z轴坐标必须相同，已自动将终点位置的z轴坐标设为与起点相同")
 
-    # TODO: 计算过的点将会添加到此处
+    # 计算过的点将会添加到此处
     computed_points = []
     computed_points_lists = []
     closed_points_lists = []
